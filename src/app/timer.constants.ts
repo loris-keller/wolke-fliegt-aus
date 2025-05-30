1f5423b962b0aab4bdd52e661b072ff1eaa7e853
Loris Keller <120105457+loris-keller@users.noreply.github.com>
2025-05-30 14:34:51 +0200

    Update timer.constants.ts

--- src/app/timer.constants.ts
+++ src/app/timer.constants.ts
@@ -1,5 +1,27 @@
 export const PACKLISTE_DATE = new Date(2025, 6, 17, 17, 0, 0, 0);
 
+export const PANIK_DATE = new Date(2025, 6, 17, 17, 15, 0, 0);
+
 export const STARTPUNKT_DATE = new Date(2025, 6, 18, 6, 0, 0, 0);
 
-export const ABFAHRT_DATE = new Date(2025, 4, 29, 9, 27, 0, 0);
+export const ABFAHRT_DATE = new Date(2025, 6, 18, 14, 35, 0, 0);
+
+export const UMSTIEG_DATE = new Date(2025, 6, 18, 14, 55, 0, 0);
+
+export const ENDBAHNHOF_DATE = new Date(2025, 6, 18, 18, 0, 0, 0);
+
+export const OPTION_SPORTLICH_DATE = new Date(2025, 6, 18, 18, 30, 0, 0);
+
+export const OPTION_GUESSER_DATE = new Date(2025, 6, 18, 18, 30, 0, 0);
+
+export const HOTEL_DATE = new Date(2025, 6, 18, 18, 30, 0, 0);
+
+export const TIPPS_TRICKS_DATE = new Date(2025, 6, 18, 18, 30, 0, 0);
+
+export const HEIWEH_DATE = new Date(2025, 6, 18, 18, 30, 0, 0);
+
+export const MORITZ_DATE = new Date(2025, 6, 18, 18, 30, 0, 0);
+
+export const NATASCIA_LORIS_DATE = new Date(2025, 6, 18, 18, 30, 0, 0);
+
+export const BEATRICE_THOMAS_DATE = new Date(2025, 6, 18, 18, 30, 0, 0);
