fb706bd6ba0eb356ec292fdc25a268602cc81df5
Loris Keller <loris.keller@outlook.com>
2025-06-01 10:45:35 +0200

    Update event dates, enhance navigation styling, and improve tips section formatting

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,17 +1,17 @@
-export const PACKLISTE_DATE = new Date('2025-05-30T21:00:00+02:00');
-export const PANIK_DATE = new Date('2025-05-30T21:05:00+02:00');
-export const STARTPUNKT_DATE = new Date('2025-05-30T21:10:00+02:00');
-export const ABFAHRT_DATE = new Date('2025-05-30T21:15:00+02:00');
-export const UMSTIEG_DATE = new Date('2025-05-30T21:20:00+02:00');
-export const ENDBAHNHOF_DATE = new Date('2025-05-30T21:25:00+02:00');
-export const OPTION_SPORTLICH_DATE = new Date('2025-05-30T21:30:00+02:00');
-export const OPTION_GNUESSER_DATE = new Date('2025-05-30T21:35:00+02:00');
-export const HOTEL_DATE = new Date('2025-05-30T21:40:00+02:00');
-export const TIPPS_TRICKS_DATE = new Date('2025-05-30T21:45:00+02:00');
-export const HEIMWEH_DATE = new Date('2025-05-30T21:50:00+02:00');
-export const MORITZ_DATE = new Date('2025-05-30T21:55:00+02:00');
-export const NATASCIA_LORIS_DATE = new Date('2025-05-30T22:00:00+02:00');
-export const BEATRICE_THOMAS_DATE = new Date('2025-05-30T22:05:00+02:00');
+export const PACKLISTE_DATE = new Date('2025-07-25T17:00:00+02:00');
+export const STARTPUNKT_DATE = new Date('2025-07-25T17:05:00+02:00');
+export const PANIK_DATE = new Date('2025-07-25T17:10:00+02:00');
+export const ABFAHRT_DATE = new Date('2025-07-25T17:15:00+02:00');
+export const UMSTIEG_DATE = new Date('2025-07-25T17:20:00+02:00');
+export const ENDBAHNHOF_DATE = new Date('2025-07-25T17:25:00+02:00');
+export const OPTION_SPORTLICH_DATE = new Date('2025-07-25T17:30:00+02:00');
+export const OPTION_GNUESSER_DATE = new Date('2025-07-25T17:30:00+02:00');
+export const HOTEL_DATE = new Date('2025-07-25T17:35:00+02:00');
+export const TIPPS_TRICKS_DATE = new Date('2025-07-25T17:40:00+02:00');
+export const HEIMWEH_DATE = new Date('2025-07-25T17:50:00+02:00');
+export const MORITZ_DATE = new Date('2025-07-25T17:55:00+02:00');
+export const NATASCIA_LORIS_DATE = new Date('2025-07-25T17:55:00+02:00');
+export const BEATRICE_THOMAS_DATE = new Date('2025-07-25T17:55:00+02:00');
 
 export type Page = { name: string; path: string };
 
@@ -19,8 +19,8 @@ export type DatePage = { date: Date } & Page;
 
 export const DATE_PAGE_MAP: readonly DatePage[] = [
   { date: PACKLISTE_DATE, name: 'Packliste', path: 'packliste' },
-  { date: PANIK_DATE, name: 'Panik? Keis Problem', path: 'panik' },
   { date: STARTPUNKT_DATE, name: 'Startpunkt', path: 'startpunkt' },
+  { date: PANIK_DATE, name: 'Panik? Keis Problem', path: 'panik' },
   { date: ABFAHRT_DATE, name: 'Abfahrt', path: 'abfahrt' },
   { date: UMSTIEG_DATE, name: 'Umstiige', path: 'umstig' },
   { date: ENDBAHNHOF_DATE, name: 'Endbahnhof', path: 'endbahnhof' },
